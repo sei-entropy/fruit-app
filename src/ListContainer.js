e8640b26807e67fd9c507f8fff6e7bae99d48405
Usman Bashir <me@usmanbashir.com>
2020-02-18 14:29:58 +0300

    Connect List to Data

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ListItem from './ListItem';
 
+const fruits = [
+  'Mango', 'Banana', 'Orange',
+  'Apple', 'Pineapple', 'Dates',
+];
+
 export default class ListContainer extends React.Component {
   render() {
+    const allFruits = fruits.map(function(fruit, index) {
+      return <ListItem name={fruit} key={index} />;
+    });
+
     return (
       <ul>
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        {allFruits}
       </ul>
     );
   }
